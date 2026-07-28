33f3463ac3e8286a88b58c4c246a82b3bb4a1334
vich21000 <vich21000@gmail.com>
2026-07-29 01:50:50 +0700

    Update google-sheet-config.js

--- google-sheet-config.js
+++ google-sheet-config.js
@@ -1,2 +1,2 @@
 export const GOOGLE_SHEET_ID = "The ID for this Google Sheet is `1dro_0QBYngT57F-0pMJTmlZz7zwlnI-G6uMGp-rNmpA`.";
-export const APPS_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwhODLVbd7I56f3fSLICEzAsyUwOY90M5WgtzZVeUxkcquH8qgx0AeIDBF07AjzR_IZOg/exec";
+export const APPS_SCRIPT_WEB_APP_URL = "https://script.google.com/macros/s/AKfycbwvIoxTY7nKNPB6Itd9FtWD95blIoy2dJ_2dLPkI_6lhY6DlvQ5KHd9KZeD924v_f8kMQ/exec";
